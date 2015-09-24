955c34c9017af54c15c7ff9a6de7e1448f5f3ff9
Ben Styles <enquiries@leeds-ebooks.co.uk>
2015-09-24 13:09:47 +0100

    Attempt to fix cors

--- cat/index.js
+++ cat/index.js
@@ -9,6 +9,11 @@ import send from './cat/mail'
 const server = http.createServer()
 
 server.on('request', (request, response) => {
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Request-Method', 'POST');
+  response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+
   if (request.method === 'POST') {
     const {query} = url.parse(request.url),
           {email} = _.fromQuery(query)
@@ -32,7 +37,10 @@ server.on('request', (request, response) => {
       })
     })
 
-  } else {
+  } else if (request.method === 'OPTIONS') {
+		response.writeHead(200)
+		response.end()
+	} else {
     console.log("[405] " + request.method + " to " + request.url)
     response.writeHead(405, "Method not supported", {'Content-Type': 'text/html'})
     response.end('<html><head><title>405 - Method not supported</title></head><body><h1>Method not supported.</h1></body></html>')
